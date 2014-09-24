2a88917da5dd39f1d3364cbf5a9813d97bcb5774
Martin Flores <cachopunk@gmail.com>
2014-09-24 18:06:43 -0300

    Investigando un poco mas vi como obtener las configuraciones de las cajas. Hay que vichar la API bien que esta compleja.

--- src/main/java/DynamicWorkberchTopologyMain.java
+++ src/main/java/DynamicWorkberchTopologyMain.java
@@ -4,10 +4,15 @@ import java.io.File;
 import java.io.IOException;
 
 import uk.org.taverna.scufl2.api.common.NamedSet;
+import uk.org.taverna.scufl2.api.common.Scufl2Tools.ProcessorSplit;
+import uk.org.taverna.scufl2.api.configurations.Configuration;
 import uk.org.taverna.scufl2.api.container.WorkflowBundle;
+import uk.org.taverna.scufl2.api.core.Processor;
 import uk.org.taverna.scufl2.api.io.ReaderException;
 import uk.org.taverna.scufl2.api.io.WorkflowBundleIO;
 import uk.org.taverna.scufl2.api.port.InputWorkflowPort;
+import uk.org.taverna.scufl2.api.profiles.Profile;
+import uk.org.taverna.scufl2.api.activity.Activity;
 
 public class DynamicWorkberchTopologyMain {
 	
@@ -15,13 +20,26 @@ public class DynamicWorkberchTopologyMain {
 		WorkflowBundleIO io = new WorkflowBundleIO();
 		File t2File = new File("/home/proyecto/Downloads/pasteur_sra.t2flow");
 		WorkflowBundle wfBundle = io.readBundle(t2File, "application/vnd.taverna.t2flow+xml");
-		NamedSet<InputWorkflowPort> inputPorts = wfBundle.getWorkflows().first().getInputPorts();
+		NamedSet<Processor> processors = wfBundle.getWorkflows().first().getProcessors();
 		
-		for (InputWorkflowPort inputWorkflowPort : inputPorts) {
+		
+		for (Profile profile : wfBundle.getProfiles()) {
+			for (Configuration config : profile.getConfigurations()) {
+				System.out.println(config.getName());
+				System.out.println(config.getType());
+				System.out.println(config.getJson());
+				
+			}
 			
-			System.out.println(inputWorkflowPort.getName());
-			
+			/*for (Activity activity : profile.getActivities()) {
+				System.out.println(activity.getName());
+				System.out.println(activity.getType());
+				
+			}*/
 		}
+		/*for (Processor processor: processors) {
+			System.out.println(processor.getName());
+		}*/
 	}
 
 }
